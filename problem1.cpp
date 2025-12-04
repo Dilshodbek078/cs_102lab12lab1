cf0502c376901c35cb9775f544e5cf01a099e0eb
Dilshodbek078 <d.orazbayev@newuu.uz>
2025-12-04 23:34:13 +0500

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream in("input.txt");
+    long long x, sum = 0;
+    while (in >> x) sum += x;
+    cout << sum;
+    return 0;
+}
