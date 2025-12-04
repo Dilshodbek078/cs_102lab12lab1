12cca820cb9feb2aa8fe2d00029135e45faa5081
Dilshodbek078 <d.orazbayev@newuu.uz>
2025-12-04 23:37:22 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+int main() {
+    int N;
+    cin >> N;
+    ofstream out("count.txt");
+    for (int i = 1; i <= N; i++) {
+        out << i;
+        if (i < N) out << " ";
+    }
+    return 0;
+}
